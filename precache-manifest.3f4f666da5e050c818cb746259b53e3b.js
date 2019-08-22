26ceb34be427d8c19bd48b1ba901b6ad39e179ad
Deniz Okcu <deniz.okcu@gmail.com>
2019-08-22 16:31:47 +0200

    Updates

---
+++ precache-manifest.3f4f666da5e050c818cb746259b53e3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057333d4816d2e56cbd127e739d2f3ad",
+    "url": "/comic-advisor/index.html"
+  },
+  {
+    "revision": "17a6af6b0820063b1970",
+    "url": "/comic-advisor/static/css/main.88c2debf.chunk.css"
+  },
+  {
+    "revision": "0e2130828309e1af2543",
+    "url": "/comic-advisor/static/js/2.b55f24e0.chunk.js"
+  },
+  {
+    "revision": "17a6af6b0820063b1970",
+    "url": "/comic-advisor/static/js/main.6c4131d6.chunk.js"
+  },
+  {
+    "revision": "f9813abf07489f7c7842",
+    "url": "/comic-advisor/static/js/runtime~main.5f5e0983.js"
+  }
+]);
