72e59324d922a92ecc18b4da60f3e388b7ae7af8
Deniz Okcu <deniz.okcu@gmail.com>
2019-08-22 05:00:39 +0200

    refactors non functional mutations in the serializer

--- src/utils/ComicsSerializer.js
+++ src/utils/ComicsSerializer.js
@@ -1,59 +1,59 @@
-// nicht der api trauen - validieren!
-// serializer bauen
+// add prop types
 
 class ComicsSerializer {
   formatPrice = priceString => priceString && `$${priceString}`;
+
   formatDate = dateString => {
     let date = new Date(Date.parse(dateString)).toDateString();
+
     return date !== "Invalid Date" ? date : "No Date available";
   };
 
   serialize = response => {
-    let comics = [];
-    response.data.results.map(item => {
-      let comic = {};
+    return response.data.results
+      .map(item => {
+        let comic = {};
 
-      try {
-        comic.coverPath = `${item.thumbnail.path}.${item.thumbnail.extension}`;
-        comic.creators = item.creators.items.map((creator, index) => {
-          creator.id = index;
-          return creator;
-        });
-        comic.writer = comic.creators.find(
-          creator =>
-            creator.role === "writer" ||
-            creator.role === "inker" ||
-            creator.role === "editor"
-        );
-        comic.characters = item.characters.items.map((character, index) => {
-          character.id = index;
-          return character;
-        });
-        comic.id = item.id;
-        comic.pageCount = item.pageCount || "-";
-        comic.price = this.formatPrice(
-          item.prices.find(price => price.type === "printPrice").price || "-"
-        );
-        comic.title = item.title || "No Title";
-        comic.description = item.description;
-        comic.purchaseLink = item.urls.find(
-          url => url.type === "purchase" || url.type === "detail"
-        ).url;
-        comic.date = this.formatDate(
-          item.dates.find(
-            date => date.type === "focDate" || date.type === "onsaleDate"
-          ).date
-        );
+        try {
+          comic.coverPath = `${item.thumbnail.path}.${item.thumbnail.extension}`;
+          comic.creators = item.creators.items.map((creator, index) => {
+            creator.id = index;
+            return creator;
+          });
+          comic.writer = comic.creators.find(
+            creator =>
+              creator.role === "writer" ||
+              creator.role === "inker" ||
+              creator.role === "editor"
+          );
+          comic.characters = item.characters.items.map((character, index) => {
+            character.id = index;
+            return character;
+          });
+          comic.id = item.id;
+          comic.pageCount = item.pageCount || "-";
+          comic.price = this.formatPrice(
+            item.prices.find(price => price.type === "printPrice").price || "-"
+          );
+          comic.title = item.title || "No Title";
+          comic.description = item.description;
+          comic.purchaseLink = item.urls.find(
+            url => url.type === "purchase" || url.type === "detail"
+          ).url;
+          comic.date = this.formatDate(
+            item.dates.find(
+              date => date.type === "focDate" || date.type === "onsaleDate"
+            ).date
+          );
+        } catch (e) {
+          console.log(e);
+        }
 
-        comics.push(comic);
-      } catch (e) {
-        console.log(e);
-      }
-    });
-    // add price and date formatter
-    // add prop types
-
-    return comics;
+        return comic;
+      })
+      .filter(
+        item => item.id && item.title && item.purchaseLink && item.coverPath
+      );
   };
 }
 
